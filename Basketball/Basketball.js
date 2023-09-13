b8be1e9d55835d7cea269d995cf9d1645627865b
kazweda <kazuaki.ueda@gmail.com>
2023-09-13 17:17:53 +0900

    Basketball.js の更新

--- Basketball/Basketball.js
+++ Basketball/Basketball.js
@@ -9,34 +9,51 @@ function OnStart()
     
     //Create the main layout
     lay = app.CreateLayout( "absolute" );
-    btnup = app.CreateImage( "Img/button-orange-up.png", 0.08, 0.18,  "button" );
+    
+   btnleft = app.CreateImage( "Img/button-orange-up.png", 0.08, 0.18, "button"  );
+    btnleft.SetOnTouch( function() { 
+	    gv.Execute( "moveleft();" );
+	 } );
+	 btnleft.Rotate( -60 );
+	 btnupleft = app.CreateImage( "Img/button-orange-up.png", 0.08, 0.18, "button"  );
+    btnupleft.SetOnTouch( function() { 
+	    gv.Execute( "moveupleft();" );
+	 } );
+	 btnupleft.Rotate( -30 );
+   btnup = app.CreateImage( "Img/button-orange-up.png", 0.08, 0.18,  "button" );
     btnup.SetOnTouch( function() { 
 	    gv.Execute( "moveup();" );
 	  } );
-	  btnleft = app.CreateImage( "Img/button-orange-left.png", 0.08, 0.18, "button"  );
-    btnleft.SetOnTouch( function() { 
-	    gv.Execute( "moveleft();" );
+	  btnupright = app.CreateImage( "Img/button-orange-up.png", 0.08, 0.18,  "button" );
+    btnupright.SetOnTouch( function() { 
+	    gv.Execute( "moveupright();" );
 	  } );
-	 btnright = app.CreateImage( "Img/button-orange-right.png", 0.08, 0.18, "button" );
+	 btnupright.Rotate( 30 );
+	 btnright = app.CreateImage( "Img/button-orange-up.png", 0.08, 0.18, "button" );
    btnright.SetOnTouch( function() { 
 	    gv.Execute( "moveright();" );
 	 } );
+	 btnright.Rotate( 60 );
 	 goalnet = app.CreateImage( "Img/goal-net.png", 0.073, 0.18, null );
 
    //Create game view and load our game.
    gv = app.CreateGameView( 1, 1 );
    gv.SetFile( "Bounce.js" );
    lay.AddChild( gv );
-    
- btnup.SetPosition( 0.815, 0.39 );
- lay.AddChild( btnup );
- btnleft.SetPosition( 0.75, 0.4 );
+  
+ btnleft.SetPosition( 0.67, 0.6 );
  lay.AddChild( btnleft );
- btnright.SetPosition( 0.88, 0.4 );
+ btnupleft.SetPosition( 0.71, 0.5 );
+ lay.AddChild( btnupleft );
+ btnup.SetPosition( 0.77, 0.46 );
+ lay.AddChild( btnup );
+ btnupright.SetPosition( 0.83, 0.5 );
+ lay.AddChild( btnupright );
+ btnright.SetPosition( 0.87, 0.6 );
  lay.AddChild( btnright );
+ 
  goalnet.SetPosition( 0.04, 0.3 );
  lay.AddChild( goalnet );
         
  app.AddLayout( lay );
- 
 }
